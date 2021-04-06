2bddde839149d876ec6038b3d6eb8fc8e313dd20
mateuskuritza <mateuskuritza@gmail.com>
2021-04-06 18:15:03 -0300

    js adicionarCartas add

--- script.js
+++ script.js
@@ -6,30 +6,41 @@ quantasCartas();
 function quantasCartas(){
     let numeroCartas = prompt("Qual o número de cartas? Precisa ser um número par para iniciar o jogo!");
 
-    while(numeroCartas%2!=0){
-        numeroCartas = prompt("Qual o número de cartas? Precisa ser um número par para iniciar o jogo!");
+    while(numeroCartas%2!=0 | numeroCartas>14){
+        numeroCartas = prompt("Qual o número de cartas? Número par menor que 15");
     }
 
     pegarBaralho(numeroCartas);
-    adicionarCartas(numeroCartas);
 }
 
 function pegarBaralho(numerodecartas){
 
-    let cartas = [];
+    cartas = [];
 
+    // pega os pares
     for( let i = 0; i<numerodecartas/2; i++){
         cartas.push(srcImgs[i]);
         cartas.push(srcImgs[i]);
     }
 
-    console.log(cartas);
-    // cartas.sort(() => Math.random() - 0.5);
-    // console.log(cartas);
+    //embaralha
+    cartas.sort(() => Math.random() - 0.5);
+
+    adicionarCartas();
 }
 
-function adicionarCartas(numerodecartas){
+function adicionarCartas(){
     
+    for(let i=0; i<cartas.length; i++){
 
+        containerCartas.innerHTML+=`
+        <div class="carta-virada none">
+            <img src="${cartas[i]}" alt="Parrot da carta virada">
+        </div>
+        <div class="carta">
+            <img onclick="virarCarta(this);" src="imgs/front.png" alt="Parrot atrás da carta">
+        </div>
+        `
+    }
     
 }
