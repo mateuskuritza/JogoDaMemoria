81b009cac730613db84413f31631c395ba44832d
mateuskuritza <mateuskuritza@gmail.com>
2021-04-06 18:03:32 -0300

    pegar pares js

--- script.js
+++ script.js
@@ -10,10 +10,26 @@ function quantasCartas(){
         numeroCartas = prompt("Qual o número de cartas? Precisa ser um número par para iniciar o jogo!");
     }
 
+    pegarBaralho(numeroCartas);
     adicionarCartas(numeroCartas);
+}
+
+function pegarBaralho(numerodecartas){
+
+    let cartas = [];
+
+    for( let i = 0; i<numerodecartas/2; i++){
+        cartas.push(srcImgs[i]);
+        cartas.push(srcImgs[i]);
+    }
+
+    console.log(cartas);
+    // cartas.sort(() => Math.random() - 0.5);
+    // console.log(cartas);
 }
 
 function adicionarCartas(numerodecartas){
     
+
     
 }
