3af5de4f8f20aefb49dfd69719842d5c1c992d51
mateuskuritza <mateuskuritza@gmail.com>
2021-04-09 16:46:53 -0300

    refatorada

--- script.js
+++ script.js
@@ -3,50 +3,42 @@ const containerCartas = document.querySelector(".container-cartas");
 const cronometro = document.querySelector(".cronometro");
 let cartaVirando = false;
 let IdInterval=0;
+let cartas = [];
+let cartasViradas = document.querySelectorAll(".carta-frente.virado");
+
 quantasCartas();
 
-function iniciarCronometro(){
+function contadorCronometro(){
     segundosPassados++;
     cronometro.innerHTML = segundosPassados;
 }
 
 function quantasCartas(){
-    containerCartas.innerHTML= null;
-    jogadas = 0;
-    acertos = 0;
-    segundosPassados = 0;
-    cronometro.innerHTML = segundosPassados;
-    clearInterval(IdInterval);
+    resetarValores();
     
-    numeroCartas = prompt("Qual o número de cartas? Número par de 4 a 14");
-    while(numeroCartas%2!=0 | numeroCartas>14 | numeroCartas<4){
+    numeroCartas = parseInt(prompt("Qual o número de cartas? Número par de 4 a 14"));
+    while(numeroCartas%2!==0 | numeroCartas>14 | numeroCartas<4){
         numeroCartas = prompt("Qual o número de cartas? Número par de 4 a 14");
     }
 
-    IdInterval = setInterval(iniciarCronometro ,1000);
+    IdInterval = setInterval(contadorCronometro ,1000);
     pegarBaralho(numeroCartas);
 }
 
 function pegarBaralho(numerodecartas){
-
-    cartas = [];
-
     // pega os pares
     for( let i = 0; i<numerodecartas/2; i++){
         cartas.push(srcImgs[i]);
         cartas.push(srcImgs[i]);
     }
-
     //embaralha
     cartas.sort(() => Math.random() - 0.5);
-
+    
     adicionarCartas();
 }
 
 function adicionarCartas(){
-    
     for(let i=0; i<cartas.length; i++){
-
         containerCartas.innerHTML+=`
         <ul class="carta-selecionada" onclick="virarCarta(this);">
             <li class="carta-frente">
@@ -58,74 +50,79 @@ function adicionarCartas(){
         </ul>
         `
     }
-    
 }
 
 
 function virarCarta(element){
-    if(element.querySelector(".carta-acertada") == undefined && cartaVirando == false){
+    if(element.querySelector(".carta-acertada") == undefined && cartaVirando === false){
         cartaVirando = true;
         jogadas++;
         virarCima(element);
 
-        const cartasViradas = document.querySelectorAll(".carta-frente.virado");
+        cartasViradas = document.querySelectorAll(".carta-frente.virado");
 
-        // conferir se são iguais
-        if(cartasViradas.length == 2){
-            if(cartasViradas[0].innerHTML == cartasViradas[1].innerHTML){
-                for(let i=0; i<2; i++){
-                    cartasViradas[i].classList.add("carta-acertada");
-                    cartasViradas[i].classList.remove("virado");
-                }
-                acertos+=2;
-            } else{
-                setTimeout(function(){
-                    for(let i=0; i<2; i++){
-                        virarBaixo(cartasViradas[i].parentNode);
-                    }
-                }, 1000);
-            }
+        if(cartasViradas.length === 2){
+            conferirPar();
         }
 
-        if(acertos == numeroCartas){
+        if(acertos === numeroCartas){
             setTimeout(acertouTodas,50);
         }
 
         // esperar a carta terminar de virar antes de poder virar outra
         setTimeout(() => { cartaVirando = false;},1000);
-
     }
 }
 
 function acertouTodas(){
     alert(`Você ganhou em ${jogadas} jogadas com ${segundosPassados} segundos!`);
     const pergunta = prompt("Quer jogar mais uma vez? Se quer, digite sim");
-    if(pergunta.toLowerCase() == "sim"){
+    if(pergunta.toLowerCase() === "sim"){
         quantasCartas();
     }else{
-        containerCartas.innerHTML= null;
-        segundosPassados = 0;
-        cronometro.innerHTML = segundosPassados;
-        clearInterval(IdInterval);
+        resetarValores();
         setTimeout(alert, 50, "Até a próxima!");
     }
 }
 
 function virarBaixo(element){
-
     const cartaFrente = element.querySelector(".carta-frente");
     const cartaVerso = element.querySelector(".carta-verso");
     cartaFrente.style.transform = "rotateY(180deg)";
     cartaFrente.classList.remove("virado");
     cartaVerso.style.transform = "rotateY(0deg)";
-
 }
 
 function virarCima(element){
-
     const cartaFrente = element.querySelector(".carta-frente");
     const cartaVerso = element.querySelector(".carta-verso");
     cartaFrente.style.transform = "rotateY(0deg)";
     cartaFrente.classList.add("virado");
     cartaVerso.style.transform = "rotateY(-180deg)";
+}
+
+function resetarValores(){
+    containerCartas.innerHTML= null;
+    cartas = [];
+    jogadas = 0;
+    acertos = 0;
+    segundosPassados = 0;
+    cronometro.innerHTML = segundosPassados;
+    clearInterval(IdInterval);
+}
+
+function conferirPar(){
+    if(cartasViradas[0].innerHTML === cartasViradas[1].innerHTML){
+        for(let i=0; i<2; i++){
+            cartasViradas[i].classList.add("carta-acertada");
+            cartasViradas[i].classList.remove("virado");
+        }
+        acertos+=2;
+    }else{
+        setTimeout(function(){
+            for(let i=0; i<2; i++){
+                virarBaixo(cartasViradas[i].parentNode);
+            }
+        }, 1000);
+    }
 }
